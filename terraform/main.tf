4ea9d889d1bdb2d490ab868ab5f7dea7775650f0
Miguel Angel Narvaiz <naesman1@gmail.com>
2026-01-26 21:45:53 -0600

    gitops with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,23 @@ resource "google_service_account" "log_archiver_sa" {
   account_id   = "log-archiver-sa"
   display_name = "Log Archiver Service Account"
 }
+# 4. Artifact Registry
+# Create the repository to store Docker images
+resource "google_artifact_registry_repository" "app_repo" {
+  location      = var.region
+  repository_id = "log-archiver-repo"
+  description   = "Docker repository for the log archiver app"
+  format        = "DOCKER"
+}
 
+# Grant Artifact Registry Writer role to the Service Account
+# This fixes the 'Permission denied' error during the CD push
+resource "google_artifact_registry_repository_iam_member" "repo_writer" {
+  location   = google_artifact_registry_repository.app_repo.location
+  repository = google_artifact_registry_repository.app_repo.name
+  role       = "roles/artifactregistry.writer"
+  member     = "serviceAccount:${google_service_account.log_archiver_sa.email}"
+}
 # Permission to read/pull from the Pub/Sub subscription
 resource "google_pubsub_subscription_iam_member" "subscriber_role" {
   subscription = google_pubsub_subscription.log_sub.name
